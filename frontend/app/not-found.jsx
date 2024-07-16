1f1330564a9eaf54b9d6a281a9e389fb67c43c50
amaanarivani <amaan@arivani.net>
2024-07-16 11:20:01 +0530

    not found

--- frontend/app/not-found.jsx
+++ frontend/app/not-found.jsx
@@ -1,13 +1,14 @@
-'use client'
-
-
 import { Button } from "flowbite-react"
 import Link from "next/link"
 
-export default function NotFound() {
-    return <div className="">
+const NotFound = () => {
+    return (
+    <div className="">
         <Link href='/'><Button className="ms-4" color='purple'>Back To Home</Button></Link>
         <h1 className="text-center text-4xl font-bold">404 | Page Not Found</h1>
         <img className="block m-auto h-auto max-w-full rounded-lg" src="https://cdn.svgator.com/images/2024/04/electrocuted-caveman-animation-404-error-page.gif" alt="" />
     </div>
+    )
 }
+
+export default NotFound;
